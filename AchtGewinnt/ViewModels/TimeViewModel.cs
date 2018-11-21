7532266d12903d447d8a9b5c15080c5aef18dc51
Joscha <JoschaHemlep@gmail.com>
2018-11-21 23:23:30 +0100

    WIP: Timer

--- AchtGewinnt/ViewModels/TimeViewModel.cs
+++ AchtGewinnt/ViewModels/TimeViewModel.cs
@@ -1,9 +1,38 @@
-﻿using ReactiveUI;
+﻿using System.Reactive;
+using ReactiveUI;
 
 namespace AchtGewinnt.ViewModels
 {
     public class TimeViewModel : ReactiveObject, IRoutableViewModel
     {
+
+        private void StartWorkTimer()
+        {
+
+        }
+
+        private void PauseWorkTimer()
+        {
+
+        }
+
+        private void ResetWorkTimer()
+        {
+
+        }
+
+        public TimeViewModel()
+        {
+
+            StartWorkTimerCommand = ReactiveCommand.Create(() => StartWorkTimer());
+            StopWorkTimerCommand = ReactiveCommand.Create(() => PauseWorkTimer());
+            ResetWorkTimerCommand = ReactiveCommand.Create(() => ResetWorkTimer());
+        }
+
+        public ReactiveCommand<Unit, Unit> StartWorkTimerCommand { get; }
+        public ReactiveCommand<Unit, Unit> StopWorkTimerCommand { get; }
+        public ReactiveCommand<Unit, Unit> ResetWorkTimerCommand { get; }
+
         public string UrlPathSegment { get; } = nameof(TimeViewModel);
         public IScreen HostScreen { get; }
     }
